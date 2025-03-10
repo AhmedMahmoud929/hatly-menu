6f7629a5b52b2f2c2f2b3f820e98621c02129c76
Ahmed Mahmoud <alrba2003@gmail.com>
2025-03-10 22:39:03 +0200

    fix: sync the cart icon on the header

--- components/home/menu-header.tsx
+++ components/home/menu-header.tsx
@@ -6,10 +6,11 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { LanguageSelector } from "./language-selector";
 import { ThemeToggle } from "../theme-toggle";
+import { useOrder } from "@/contexts/order-context";
 
 export function MenuHeader() {
   const { theme, setTheme } = useTheme();
-
+  const { order } = useOrder();
   return (
     <header className="sticky top-0 z-50 border-b bg-[#151515] backdrop-blur">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -20,13 +21,15 @@ export function MenuHeader() {
         <div className="flex items-center gap-2">
           {/* <LanguageSelector /> */}
           <ThemeToggle variant="outline" className="h-10 w-10" />
-          <Button variant="outline" size="icon" className="relative">
-            <ShoppingBag className="h-5 w-5" />
-            <span className="sr-only">Shopping cart</span>
-            <span className="absolute -right-1 -top-1 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
-              0
-            </span>
-          </Button>
+          <Link href={"/checkout"}>
+            <Button variant="outline" size="icon" className="relative">
+              <ShoppingBag className="h-5 w-5" />
+              <span className="sr-only">Shopping cart</span>
+              <span className="absolute -right-1 -top-1 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
+                {order?.items.length || 0}
+              </span>
+            </Button>
+          </Link>
         </div>
       </div>
     </header>
